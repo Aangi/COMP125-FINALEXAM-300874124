ea9ee6a40a07c84b5118acf133c720e0ed539c1f
Aangi <aashah.6198@gmail.com>
2016-08-17 14:24:29 -0400

    Final Commit

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,3 +1,11 @@
+/*
+Filename : App.js
+@author Aayushi Shah
+@date August 17,2016
+StudentID : 300874124
+website : http://comp125-finalexam-300874124.azurewebsites.net/
+@description : This file is the main javascript file for this web site
+*/
 var core;
 (function (core) {
     "use strict";
@@ -13,9 +21,55 @@ var core;
         main();
     }
     function gameLoop() {
+
         stage.update();
     }
     function main() {
+       var button1 = new createjs.Bitmap("../Assets/images/blank.png")
+        stage.addChild(button1);
+        var button2 = new createjs.Bitmap("../Assets/images/blank.png")
+        stage.addChild(button2);
+        var Dice1 = new createjs.Bitmap("../Assets/images/1.png");
+        stage.addChild(Dice1);
+        var Dice2 = new createjs.Bitmap("../Assets/images/2.png");
+        stage.addChild(Dice2);
+        var Dice3 = new createjs.Bitmap("../Assets/images/3.png");
+        stage.addChild(Dice3);
+        var Dice4 = new createjs.Bitmap("../Assets/images/4.png");
+        stage.addChild(Dice4);
+        var Dice5 = new createjs.Bitmap("../Assets/images/5.png");
+        stage.addChild(Dice5);
+        var Dice6 = new createjs.Bitmap("../Assets/images/6.png");
+        stage.addChild(Dice6);
+        button1.x=107.5;
+        button1.y=105;
+        button2.x=352.5;
+        button2.y=105;
+        Dice1.x = 352.5;
+        Dice1.y = 105;
+        Dice2.x = 352.5;
+        Dice2.y = 105;
+        Dice3.x = 352.5;
+        Dice3.y = 105;
+        Dice4.x = 352.5;
+        Dice4.y = 105;
+        Dice5.x = 352.5;
+        Dice5.y = 105;
+        Dice6.x = 352.5;
+        Dice6.y = 105;
+        //label object
+         
+        //button bitmap
+        
+       var rollButton = new createjs.Bitmap("../Assets/images/rollButton.png");
+        stage.addChild(rollButton);
+        rollButton.x=250;
+        rollButton.y=350;
+    }
+
+    function roll(){
+        console.log("clicked");
+        var randomNumber = Math.floor(Math.random()* 6);
     }
     window.addEventListener("load", init);
 })(core || (core = {}));
